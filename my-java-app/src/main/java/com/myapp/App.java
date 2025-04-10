71559bd03d7b43686181f0232490dcff3ec4e5a8
Riyazuddin <21050074+Riyazkhan14@users.noreply.github.com>
2025-04-10 19:16:22 +0530

    Update App.java

--- my-java-app/src/main/java/com/myapp/App.java
+++ my-java-app/src/main/java/com/myapp/App.java
@@ -2,7 +2,7 @@ package com.myapp;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello, Maven! from Riyaz");
+        System.out.println("Hello, Maven! from Riyaz, Small change in code for automated pipeline.");
     }
 
     public static int add(int a, int b) {
