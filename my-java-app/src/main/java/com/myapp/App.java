ba475f95dede616f6baf9ddd9db1366978735ade
Riyazkhan14 <21050074+Riyazkhan14@users.noreply.github.com>
2025-04-10 16:33:31 +0530

    Made small changes in source code

--- my-java-app/src/main/java/com/myapp/App.java
+++ my-java-app/src/main/java/com/myapp/App.java
@@ -2,7 +2,7 @@ package com.myapp;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello, Maven!");
+        System.out.println("Hello, Maven! from Riyaz");
     }
 
     public static int add(int a, int b) {
